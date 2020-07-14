f19ece3f0134f8530153a03e672224adabc332a9
Jordan <jayxale@icloud.com>
2020-07-13 19:42:02 -0700

    database

---
+++ migrations/20200714023614-create-users.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.STRING
+      },
+      Sender: {
+        type: Sequelize.STRING
+      },
+      Recipient: {
+        type: Sequelize.STRING
+      },
+      MessagePrivate: {
+        type: Sequelize.STRING
+      },
+      MessagePublic: {
+        type: Sequelize.STRING
+      },
+      Username: {
+        type: Sequelize.STRING
+      },
+      Password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-/* GET users listing. */
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
 });
+
 
 module.exports = router;
