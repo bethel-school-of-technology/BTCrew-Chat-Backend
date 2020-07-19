1f4effb1072334f19aee202abbc05b8c8dcdbae6
Marion <thomasm1124@outlook.com>
2020-07-19 12:24:14 -0400

    dependencies

---
+++ migrations/20200710194448-create-users.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      Sender: {
+        type: Sequelize.STRING
+      },
+      Recipient: {
+        type: Sequelize.STRING
+      },
+      MessagePrivate: {
+        type: Sequelize.STRING
+      },
+      MessagePublic: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
